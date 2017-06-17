21401e0d836aebf610f92926e0b0e16b338dac31
deeze307 <deeze.designs@gmail.com>
2017-06-16 23:00:23 -0300

    listas detalles

--- src/pages/ofertaslaborales/ofertaslaborales-detalle.ts
+++ src/pages/ofertaslaborales/ofertaslaborales-detalle.ts
@@ -9,14 +9,21 @@ export class OfertasLaboralesDetalle {
     drawerOptions: any;
     public backgroundImage = "assets/img/background/BG_TeU_SIN_TEXTO.jpg";
     public backgroundCard = "assets/img/background/consejoBG.jpg";
-    item;
+    item: Array<{ titulo: string, descripcion: any, movil:string, email: any, id_categoria: number, craeted_at: string }>;
     showMovil;
     showEmail;
 
   constructor(params: NavParams) {
       this.showMovil = false;
       this.showEmail = false;
-      this.item = params.data.item;
+      this.item = [
+            titulo = params.data.item.titulo,
+            descripcion = params.data.item.descripcion.split('\n'),
+            movil = params.data.item.movil,
+            email = params.data.item.email,
+            id_categoria = params.data.item.id_categoria,
+            created_at = params.data.item.created_at,
+        ];
 
       if(this.item.movil == null){
           this.item.movil = "N/A";
@@ -25,6 +32,7 @@ export class OfertasLaboralesDetalle {
       if(this.item.email == null){
           this.item.email = "N/A";
       }
+      console.log(this.item);
   }
 
   ionViewDidLoad() {
