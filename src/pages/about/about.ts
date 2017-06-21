d9423b513f9996fa038cb0ed153bada78f6623fb
deeze307 <deeze.designs@gmail.com>
2017-06-21 16:02:54 -0300

    ..

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,16 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {LoadingController} from "ionic-angular/index";
+import {TeuServiceProvider} from "../../providers/teu-service/teu-service";
+
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+    loading:any;
+    staff: any;
+    preStaff:Array<{nombre:string,apellido:string,telefono:string,email:string,id_rol:string,descripcion:string}>;
   public backgroundImage = "assets/img/background/BG_TeU_SIN_TEXTO.jpg";
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public teuServiceProvider: TeuServiceProvider, public loadingCtrl: LoadingController) {
+      this.presentLoading();
+      this.getStaff();
   }
 
+  presentLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Cargando...',
+      duration: 10000
+    });
+    this.loading.present();
+  }
+
+  getStaff(){
+      this.teuServiceProvider.getStaff().then(dataObject=>{
+          this.staff=[];
+          for(var data in dataObject){
+              this.preStaff = [{
+                  nombre: dataObject[data].nombre,
+                  apellido: dataObject[data].apellido,
+                  telefono: dataObject[data].telefono,
+                  email: dataObject[data].email,
+                  id_rol: dataObject[data].id_rol,
+                  descripcion: dataObject[data].descripcion,
+              }];
+              this.staff.push(this.preStaff);
+          };
+          console.log(this.staff);
+
+          this.loading.dismiss();
+      })
+  }
 }
