5c6a0b27e7244fe9975f3b416c598e60a8a6bb51
deeze307 <deeze.designs@gmail.com>
2017-06-25 01:44:10 -0300

    cambios rusticos

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -4,58 +4,8 @@ import { AppState } from './app.global';
 // Home Page
 import { HomePage } from '../pages/_home/home';
 
-import { IonicOfficialComponentsPage } from '../pages/ionic-official-components/ionic-official-components';
-
 // Theme
 import { ThemingPage } from '../pages/theming/theming';
-
-// Profile list
-import { ProfileListPage } from '../pages/profile/profile';
-import { ProfileOnePage } from '../pages/profile/profile-one/profile-one';
-import { ProfileTwoPage } from '../pages/profile/profile-two/profile-two';
-import { ProfileThreePage } from '../pages/profile/profile-three/profile-three';
-import { ProfileFourPage } from '../pages/profile/profile-four/profile-four';
-import { ProfileSettingsPage } from '../pages/profile/profile-settings/profile-settings';
-
-// Lists list
-import { ListsPage } from '../pages/list/list';
-import { SlidingItemListPage } from '../pages/list/sliding-item/sliding-item';
-import { SettingsListPage } from '../pages/list/settings/settings';
-
-// Login
-import { LoginListPage } from '../pages/login/login';
-import { LoginOnePage } from '../pages/login/login-one/login-one';
-import { LoginInstagramPage } from '../pages/login/login-instagram/login-instagram';
-import { LoginSliderPage } from '../pages/login/login-slider/login-slider';
-import { LoginBackgroundVideoPage } from '../pages/login/login-background-video/login-background-video';
-import { LoginBackgroundSliderPage } from '../pages/login/login-background-slider/login-background-slider';
-
-// Popup Menu list
-import { PopupMenuListPage } from '../pages/popup-menu/popup-menu';
-import { PopupMenuOnePage } from '../pages/popup-menu/popup-menu-one/popup-menu-one';
-
-// Cards list
-import { CardListPage } from '../pages/ionic-official-components/card/card';
-import { CardSocialPage } from '../pages/ionic-official-components/card/card-social/card-social';
-import { CardImagePage } from '../pages/ionic-official-components/card/card-image/card-image';
-import { CardMapPage } from '../pages/ionic-official-components/card/card-map/card-map';
-import { CardBackgroundPage } from '../pages/ionic-official-components/card/card-background/card-background';
-import { CardBadgePage } from '../pages/ionic-official-components/card/card-badge/card-badge';
-
-// Buttons list
-import { ButtonsListPage } from '../pages/ionic-official-components/button/button';
-import { ButtonStylesPage } from '../pages/ionic-official-components/button/button-styles/button-styles';
-import { IconButtonsPage } from '../pages/ionic-official-components/button/icon-buttons/icon-buttons';
-import { ButtonComponentsPage } from '../pages/ionic-official-components/button/button-components/button-components';
-
-// Modals
-import { PopupModalsPage } from '../pages/popup-modal/popup-modal';
-import { HintModalPage } from '../pages/popup-modal/hint-modal/hint-modal';
-import { SignupModalPage } from '../pages/popup-modal/signup-modal/signup-modal';
-import { WalkthroughModalPage } from '../pages/popup-modal/walkthrough-modal/walkthrough-modal';
-
-// Alerts
-import { AlertsPage } from '../pages/ionic-official-components/alert/alert';
 
 // Contacto
 import { ContactPage} from '../pages/contact/contact';
@@ -82,46 +32,7 @@ import { Sorteos } from '../pages/sorteos/sorteos';
 
 import { Ofrecimientos } from '../pages/ofrecimientos/ofrecimientos';
 
-// Slides
-import { SlidesPage } from '../pages/slide/slide';
-import { SlideTransitionsPage } from '../pages/slide/slide-transitions/slide-transitions';
-import { SlideCarouselPage } from '../pages/slide/slide-carousel/slide-carousel';
-import { SlidePhotoGalleryPage } from '../pages/slide/slide-photo-gallery/slide-photo-gallery';
-import { SlideCustomPaginationPage } from '../pages/slide/slide-custom-pagination/slide-custom-pagination';
-import { SliderListTwoPage } from '../pages/slide/slider-list-two/slider-list-two';
-import { SliderListPage } from '../pages/slide/slider-list/slider-list';
-import { SlideMultirowPage } from '../pages/slide/slide-multirow/slide-multirow';
-import { SlideRightToLeftPage } from '../pages/slide/slide-rtl/slide-rtl';
-import { SlideFreeModePage } from '../pages/slide/slide-free-mode/slide-free-mode';
-import { SlideNestedPage } from '../pages/slide/slide-nested/slide-nested';
-import { SlideColorChangingPage } from '../pages/slide/slide-color-changing/slide-color-changing';
-
-// Miscellaneous list
-import { MiscellaneousListPage } from '../pages/miscellaneous/miscellaneous';
-import { ClockPage } from '../pages/miscellaneous/clock/clock';
-import { CountdownOnePage } from '../pages/miscellaneous/countdown/countdown';
-import { TinderCardsPage } from '../pages/miscellaneous/tinder-cards/tinder-cards';
-import { TestimonialsPage } from '../pages/miscellaneous/testimonials/testimonials';
-import { CurrencyConverterPage } from '../pages/miscellaneous/currency-converter/currency-converter';
-import { WeatherPage } from '../pages/miscellaneous/weather/weather';
-import { MessagesPage } from '../pages/miscellaneous/chat/messages/messages';
-import { ChatsPage } from '../pages/miscellaneous/chat/chats';
-import { BlogPostPage } from '../pages/miscellaneous/blog-post/blog-post';
-import { CreditCardScanPage } from '../pages/miscellaneous/credit-card-scan/credit-card-scan';
-import { RuntimePermissionsPage } from '../pages/miscellaneous/runtime-permissions/runtime-permissions';
-import { ChartsPage } from '../pages/miscellaneous/charts/charts';
-import { GetImagePage } from '../pages/miscellaneous/get-image/get-image';
-import { BarcodescannerPage } from '../pages/miscellaneous/barcodescanner/barcodescanner';
-import { ExpandableHeaderPage } from '../pages/miscellaneous/expandable-header/expandable-header';
-import { FlashCardPage } from '../pages/miscellaneous/flash-card/flash-card';
-import { SideMenuPage } from '../pages/side-menu/side-menu';
-import { AutosizingTextarea } from '../pages/miscellaneous/autosizing-textarea/autosizing-textarea';
-
 // Providers
-import { WeatherService } from '../pages/miscellaneous/weather/weather.service';
-import { ToastService } from '../providers/util/toast.service';
-import { AlertService } from '../providers/util/alert.service';
-import { CameraProvider } from '../providers/util/camera.provider';
 import { TeuServiceProvider } from '../providers/teu-service/teu-service';
 
 // Ionic native providers
@@ -169,23 +80,6 @@ export const Pages = [
     // Theming
     ThemingPage,
 
-    // Ionic Official Components
-    IonicOfficialComponentsPage,
-
-    // Buttons
-    ButtonsListPage,
-    IconButtonsPage,
-    ButtonComponentsPage,
-    ButtonStylesPage,
-
-    // Lists
-    ListsPage,
-    SettingsListPage,
-    SlidingItemListPage,
-
-    // Alerts
-    AlertsPage,
-
     // Contacto
     ContactPage,
 
@@ -206,78 +100,6 @@ export const Pages = [
     // Ofrecimientos
     Ofrecimientos,
 
-
-    // Cards
-    CardListPage,
-    CardBackgroundPage,
-    CardMapPage,
-    CardImagePage,
-    CardSocialPage,
-    CardBadgePage,
-
-    // Profile
-    ProfileListPage,
-    ProfileOnePage,
-    ProfileTwoPage,
-    ProfileThreePage,
-    ProfileFourPage,
-    ProfileSettingsPage,
-
-    LoginListPage,
-    LoginOnePage,
-    LoginInstagramPage,
-    LoginSliderPage,
-    LoginBackgroundVideoPage,
-    LoginBackgroundSliderPage,
-
-    // Side Menu
-    SideMenuPage,
-
-    // Miscellaneous
-    MiscellaneousListPage,
-    PopupMenuListPage,
-    PopupMenuOnePage,
-    TestimonialsPage,
-    TinderCardsPage,
-    CountdownOnePage,
-    CurrencyConverterPage,
-    CreditCardScanPage,
-    ClockPage,
-    WeatherPage,
-    ChatsPage,
-    MessagesPage,
-    BlogPostPage,
-    ChartsPage,
-    RuntimePermissionsPage,
-    GetImagePage,
-    FlashCardPage,
-    BarcodescannerPage,
-    ExpandableHeaderPage,
-    AutosizingTextarea,
-
-    // Modals
-    PopupModalsPage,
-    SignupModalPage,
-    WalkthroughModalPage,
-    HintModalPage,
-
-    // Slides
-    SlidesPage,
-    SlideCarouselPage,
-    SlideTransitionsPage,
-    SlidePhotoGalleryPage,
-    SlideNestedPage,
-    SlideFreeModePage,
-    SlideRightToLeftPage,
-    SlideMultirowPage,
-    SliderListPage,
-    SliderListTwoPage,
-    SlideCustomPaginationPage,
-    SlideColorChangingPage,
-
-    // Popup Menu
-    PopupMenuListPage,
-    PopupMenuOnePage,
 ];
 
 export const Pipes = [
@@ -285,11 +107,7 @@ export const Pipes = [
 ];
 
 export const Providers = [
-    WeatherService,
-    AlertService,
-    ToastService,
     AppState,
-    CameraProvider,
     TeuServiceProvider,
 
     // Ionic native specific providers
